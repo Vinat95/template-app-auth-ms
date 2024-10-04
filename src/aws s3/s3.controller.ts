35fa0cf2ccf428bcede648537b7c77e29076d0e7
Vincenzo Attanasio <vinat2000@gmail.com>
2024-10-04 17:57:40 +0200

    gestione errore foto

--- src/aws s3/s3.controller.ts
+++ src/aws s3/s3.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Controller,
   Delete,
   Param,
@@ -15,7 +16,7 @@ export class UploadController {
   constructor(private readonly s3Service: S3Service) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor("file")) // 'file' è il nome del campo dell'input file
+  @UseInterceptors(FileInterceptor("file"))
   @ApiConsumes("multipart/form-data")
   @ApiBody({
     description: "File to upload",
@@ -44,8 +45,21 @@ export class UploadController {
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     let result;
     if (file) {
-      result = await this.s3Service.uploadImageToS3(file);
+      if (file.mimetype !== "image/jpeg") {
+        throw new BadRequestException("Solo i file JPEG sono consentiti");
+      }
+      if (file.size > 100 * 1024) {
+        throw new BadRequestException("Il file supera i 100Kb");
+      }
+      // Caricamento del file su S3
+      try {
+        result = await this.s3Service.uploadImageToS3(file);
+      } catch (error) {
+        // Gestione dell'errore di caricamento
+        throw new BadRequestException("Errore durante il caricamento del file");
+      }
     }
+
     return {
       message: "File uploaded successfully",
       url: result ? result.Location : "", // URL dell'immagine caricata
